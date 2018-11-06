e13f4eae56cae272d1f7310e91b89c02eac31ff6
Chris Cosnowski <chris.cosnowski@gmail.com>
2018-11-05 21:35:09 -0500

    Third Commit

--- Wpf_SimpleCalculator/CalculatorWindow.xaml.cs
+++ Wpf_SimpleCalculator/CalculatorWindow.xaml.cs
@@ -16,13 +16,84 @@ using System.Windows.Shapes;
 namespace Wpf_SimpleCalculator
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for CalculatorWindow.xaml
     /// </summary>
     public partial class CalculatorWindow : Window
     {
+        //
+        //Variables
+        int num1 = 0;
+        int num2 = 0;
+        string Operator = "";
+
         public CalculatorWindow()
         {
             InitializeComponent();
         }
+
+        //
+        //When clicked, sets the Text Box to "0"
+        private void btn_Clear_Click(object sender, RoutedEventArgs e)
+        {
+            txtBox_Main.Text = "0";
+            Operator = "";
+        }
+
+        //
+        //A Switch Statement to tell what math to do when Equals is clicked
+        private void btn_Equal_Click(object sender, RoutedEventArgs e)
+        {
+            num2 = int.Parse(txtBox_Main.Text);
+
+            switch (Operator)
+            {
+                case "+":
+                    txtBox_Main.Text = (num1 + num2).ToString();
+                    break;
+                case "-":
+                    txtBox_Main.Text = (num1 - num2).ToString();
+                    break;
+                case "*":
+                    txtBox_Main.Text = (num1 * num2).ToString();
+                    break;
+            }
+        }
+
+        //
+        //Not sure what this is exactly, it forced me to put this or the 
+        //application would not run
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        //
+        //Reads whats in the text box
+        private void input(string a)
+        {
+            if (txtBox_Main.Text == "0")
+                txtBox_Main.Text = a;
+            else
+                txtBox_Main.Text += a;
+        }
+
+        //
+        //Acts like a lister for when a number is clicked
+        private void btn_Number_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn_Number = (Button)sender;
+            input(btn_Number.Content.ToString());
+        }
+
+        //
+        //Defines the Operator functions: -, *, +
+        private void btn_Operator_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn_Operator = (Button)sender;
+            Operator = btn_Operator.Content.ToString();
+            num1 = int.Parse(txtBox_Main.Text);
+            txtBox_Main.Text = "0";
+        }
     }
 }
+
